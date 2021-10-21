1debbaa7cb1024a7460f7548b5a35c426b9638ba
unknown <vothao.tin@gmail.com>
2021-10-21 11:55:43 +0700

    up

---
+++ display.sql
@@ -0,0 +1,29 @@
+USE quanlysinhvien;
+
+SELECT * FROM Student ;
+
+SELECT * FROM Student
+WHERE Status = true;
+
+SELECT * FROM Subject
+WHERE Credit < 10;
+
+SELECT Student.StudentId, Student.StudentName, class.ClassID
+FROM Student  
+JOIN Class  on Student.ClassId = Class.ClassID;
+
+SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S
+JOIN Class C on S.ClassId = C.ClassID
+WHERE C.ClassName = 'A1';
+
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S 
+JOIN Mark M on S.StudentId = M.StudentId 
+JOIN Subject Sub on M.SubId = Sub.SubId ;
+
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S 
+JOIN Mark M on S.StudentId = M.StudentId 
+JOIN Subject Sub on M.SubId = Sub.SubId
+WHERE Sub.SubName = 'CF';
